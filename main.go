0b899808732cdd8cb6b32b86a9912816183b481a
default <miles.aube@stu.ocsb.ca>
2025-09-16 23:04:21 +0000

    added my full name program in go and ts

---
+++ main.go
@@ -0,0 +1,14 @@
+/*
+* Author: Miles Aube 
+* Version: 1.0.0
+* Date: 2025-09-16
+* This is my full name program
+*/
+package main
+
+import "fmt"
+
+func main () {
+			// print out my full name 
+			fmt.Println("Miles Aube")
+}
---
+++ main.ts
@@ -0,0 +1,9 @@
+/**
+ * @author Miles Aube 
+ * @version 1.0.0
+ * @date 2025-09-16
+ * @fileoverview This is a program to type out my first and last name
+ */
+
+// print out my name 
+console.log("Miles Aube");
